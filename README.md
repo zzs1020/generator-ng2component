fa08925e4bd306dc426f6cb3fe40e1dc2920fea8
zzs1020 <mailtozeshi@gmail.com>
2016-11-01 19:36:21 -0400

    added style sheets choices, changed template name

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 An angular 2 component generator
 
 **following is prepared for future, not available now**
-To use the tool, please go through these steps:
+
+To use the tool, please follow these steps:
 1. install yeoman
-> npm install -g yeoman
+    > npm install -g yeoman
 2. install my tool
-> npm install -g generator-ng2component
+    > npm install -g generator-ng2component
 3. run via yeoman
-> yo ng2component
+    > yo ng2component
 4. follow the command line instructions, and your component will be populated in designated folder.
 
-yeoman: http://yeoman.io/generators/
+Find more generators on yeoman: http://yeoman.io/generators/
--- app/index.js
+++ app/index.js
@@ -19,49 +19,56 @@ module.exports = generators.Base.extend({
         // init some variables
         this.component_name = 'show-message';
         this.component_location = 'src/app';
-        this.component_class = 'ShowMessageComponent'
+        this.component_class = 'ShowMessageComponent';
+        this.component_style = 'scss';
     },
     // this function used to interact with user
     prompting: function () {
         return this.prompt([{
             type    : 'input',
             name    : 'component_name',
-            message : 'Component folder name? [dash style]',
+            message : 'component folder name? [use kebab-case]',
             store   : true,
             default : this.component_name
         }, {
             type    : 'input',
             name    : 'component_location',
-            message : 'Where do you want put your component folder?',
+            message : 'resides position? (under current folder)',
             store   : true,
             default : this.component_location
+        }, {
+            type    : 'list',
+            name    : 'component_style',
+            choices : ['scss', 'css', 'less'],
+            message : 'style sheets?'
         }]).then(function (answers) {
             // change our instance variable
             this.component_name = answers.component_name;
             this.component_location = answers.component_location;
             this.component_class = this.toClassNameHelper(answers.component_name);
+            this.component_style = answers.component_style;
         }.bind(this));
     },
 
     // starting creating files
     writing: function () {
         this.fs.copyTpl(
-            this.templatePath('_component.html'),
+            this.templatePath('template.component.html'),
             this.destinationPath(this.component_location+'/'+ this.component_name + '/'+ this.component_name +'.component.html')
         );
         this.fs.copyTpl(
-            this.templatePath('_component.ts'),
+            this.templatePath('template.component.spec.ts'),
             this.destinationPath(this.component_location+'/'+ this.component_name + '/'+ this.component_name +'.component.ts'),
             {folder_name: this.component_name, class_name: this.component_class}
         );
         this.fs.copyTpl(
-            this.templatePath('_component.spec.ts'),
+            this.templatePath('template.component.spec.ts'),
             this.destinationPath(this.component_location+'/'+ this.component_name + '/'+ this.component_name +'.component.spec.ts'),
             {folder_name: this.component_name, class_name: this.component_class}
         );
         this.fs.copyTpl(
-            this.templatePath('_component.scss'),
-            this.destinationPath(this.component_location+'/'+ this.component_name + '/'+ this.component_name +'.component.scss')
+            this.templatePath('template.component.' + this.component_style),
+            this.destinationPath(this.component_location+'/'+ this.component_name + '/'+ this.component_name +'.component.'+ this.component_style)
         );
     }
 });
