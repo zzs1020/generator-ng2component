2ee8348472ed858061a663afe4b844ca1943bc14
Steve Zheng <steve.zheng@globallogic.com>
2016-11-09 12:22:24 -0500

    fixed weired webstorm space/tab indent issue

--- app/templates/template.component.spec.ts
+++ app/templates/template.component.spec.ts
@@ -2,17 +2,17 @@ import { TestBed } from '@angular/core/testing';
 import { <%= class_name %> } from './<%=folder_name %>.component';
 
 describe('<%= class_name %>', () => {
-	beforeEach(() => {
-		TestBed.configureTestingModule({
-			declarations: [ <%= class_name %> ]
-		});
-	});
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ <%= class_name %> ]
+        });
+    });
 
     describe('method ', () => {
-		it('should ', () => {
-		});
-	});
+        it('should ', () => {
+        });
+    });
 
     it('should ', () => {
-	});
+    });
 });
